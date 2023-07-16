d9080a26f13859e69afa1855d6c92cdc1cf790c2
DmitrijLosev <131905222+DmitrijLosev@users.noreply.github.com>
2023-07-16 16:38:04 +0300

    Posts delete,post and delete like

--- src/components/Profile/Myposts/Myposts.tsx
+++ src/components/Profile/Myposts/Myposts.tsx
@@ -11,7 +11,9 @@ let maxLength15=maxLengthCreator(15)
 const Myposts:React.FC<MapStatePropsType&MapDispatchStatePropsType> = (props) => {
 
     let postsElements = props.posts.map(p =>
-        <Post key={p.id} message={p.post} like={p.likesCount}/>)
+        <Post key={p.id} id={p.id} post={p.post} like={p.likesCount} likedPost={p.likedPost} photo={props.photo}
+              deleteChoosedPost={props.deleteChoosedPost}
+              likeOrDislikePost={props.likeOrDislikePost}/>)
 
     const onSubmit=(formData:formDataNewPostType)=>{
         props.addPost(formData.NewPostText)}
--- src/components/Profile/Myposts/MypostsContainer.tsx
+++ src/components/Profile/Myposts/MypostsContainer.tsx
@@ -8,16 +8,20 @@ import {actions} from "../../../Redux/Profile-reducer";
 export type MapStatePropsType=ReturnType<typeof mapStateToProps>
 export type MapDispatchStatePropsType={
     addPost:(NewPostText:string)=>void
+    deleteChoosedPost:(id:number)=>void
+    likeOrDislikePost:(id:number)=>void
 }
 
 let mapStateToProps=(state:AppStateType)=>{
     return{
-        posts:state.ProfilePage.posts
+        posts:state.ProfilePage.posts,
+        photo:state.ProfilePage.profile?.photos.small
     }
 }
 
 
 const MyPostsContainer=connect<MapStatePropsType,MapDispatchStatePropsType, {},AppStateType>
-(mapStateToProps, {addPost:actions.addPost})(Myposts);
+(mapStateToProps, {addPost:actions.addPost, deleteChoosedPost:actions.deleteChoosedPost,
+    likeOrDislikePost:actions.likeOrDislikePost})(Myposts);
 
 export default MyPostsContainer;
